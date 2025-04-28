98c9a7511b1f91470a1dea36fed1a2f7e5b81027
mission-arodrigues <arodrigues@missioncloud.com>
2025-04-28 08:33:32 -0700

    Implementando rotas CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,66 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get("/")
+
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World", "batata": "frita"}
+
+
+@app.get("/exercicio2", status_code=HTTPStatus.OK, response_class=HTMLResponse)  # HTML RESPONSE - response_class muda a classe de JSON pra HTML
+def exercicio2():
+    return """
+    <html>
+        <head>
+            <title> FastAPI do Deco </title>
+        </head>
+        <body>
+            <h1> Meu teste de app </h1>
+            <h2> Testando resposta HTML e PyTest de função :D </h2>
+        </body>
+    </html>"""
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)  # Referencia UserPublic pra não expor senha
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get("/users/", response_model=UserList)
+def read_user():
+    return {"users": database}
+
+@app.get("/users/{user_id}") #Exercicio
+def read_specific_user(user_id: int):
+    user = database[user_id - 1]
+    return user
+
+
+@app.put("/users/{user_id}", response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database) + 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found!")
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete("/users/{user_id}", response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database) + 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found!")
+    del database[user_id - 1]
+    return {"message": "User deleted!"}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,56 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():  # Nome descritivo
-    client = TestClient(app)  # Arrange (organização)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):  # Nome descritivo
     response = client.get("/")  # Act (açãoo,executa algo)
 
     assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
     assert response.json() == {"message": "Hello World"}
+
+
+def test_exercicio_2_retornar_ok_e_html(client):
+    response = client.get("/exercicio2")
+
+    assert response.status_code == HTTPStatus.OK
+    assert "<h1> Meu teste de app </h1>" in response.text
+
+
+def test_create_user(client):
+    response = client.post("/users/", json={"username": "test", "email": "test@example.com", "password": "testing"})
+    # Validar Response Code
+    assert response.status_code == HTTPStatus.CREATED
+    # Validar UserPublic
+    assert response.json() == {"username": "test", "email": "test@example.com", "id": 1}
+
+
+def test_read_user(client):
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"users": [{"username": "test", "email": "test@example.com", "id": 1}]}
+
+
+def test_update_user(client):
+    response = client.put("/users/1", json={"username": "icarus", "email": "bk@example.com", "password": "test-pass"})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"id": 1, "username": "icarus", "email": "bk@example.com"}
+
+
+def test_update_error(client):
+    response = client.put("/users/3", json={"username": "icarus", "email": "bk@example.com", "password": "test-pass"})
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "User deleted!"}
+
+
+def test_delete_error(client):
+    response = client.delete("users/2")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
